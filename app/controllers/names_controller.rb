2c295b3975a35406e9bd3472d771382d63467202
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-16 19:47:12 -0500

    add route to add url segment

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,6 +1,14 @@
 class NamesController < ApplicationController
 
-  def name_game
+  def guess_number
+    
     render json:{message:"Hello"}
   end
+
+  def url_segment_number_guess
+    # answer = 36
+    # if :userguess.to<
+    render json: params[:user_guess]
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/name", controller:"names", action:"name_game"
+  get "/name", controller:"names", action:"guess_number"
+  get "/number_game/:user_guess", controller:"names", action: "url_segment_number_guess"
 end
